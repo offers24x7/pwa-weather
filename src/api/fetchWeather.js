c966debdc740a7e51822e7357d471e21558f128b
Vara Prasad <prasad.marini@gmail.com>
2021-02-09 21:35:15 +0530

    removed env values

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
-const URL= process.env.REACT_APP_URL;
-const APIKEY = process.env.REACT_APP_API_KEY;
+const URL= 'https://api.openweathermap.org/data/2.5/weather';
+const APIKEY = 'b01fff83fdbe0ba5e599a06a5668747b';
 
 export const fetchWeather = async(query)=>{
     const {data} = await Axios.get(URL,
